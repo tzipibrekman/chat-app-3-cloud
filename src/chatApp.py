51d27476fc7ce49d6de0ab1b36cffbc3fab919b8
TzipiBrekman <tzipi66191@gmail.com>
2023-08-23 14:30:06 +0300

    rout

--- src/chatApp.py
+++ src/chatApp.py
@@ -17,6 +17,12 @@ def handling_request_login():
     return render_template('login.html')
 
 
+def handling_request_lobby():
+    if request.method == 'POST':
+        room=request.form['new_room']
+    return render_template('lobby.html')
+
+
 @server.route('/', methods=['GET', 'POST'])
 def homePage():
     return handling_request_register()
@@ -26,9 +32,12 @@ def register():
     return handling_request_register()
 
 @server.route('/login', methods=['GET', 'POST'])
-def loginPage():
+def login():
     return handling_request_login()
 
+@server.route('/lobby', methods=['GET', 'POST'])
+def lobby():
+    return handling_request_lobby()
 
 
 if __name__ == "__main__":
