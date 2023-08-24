117518d4663767c7c885d7cc59497913df36e5a8
TzipiBrekman <tzipi66191@gmail.com>
2023-08-24 09:47:36 +0300

    lobby and chat

--- src/chatApp.py
+++ src/chatApp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 server = Flask(__name__)
 
 def check_user_existing(name,password):
@@ -33,6 +34,20 @@ def handling_request_login():
             return redirect('/lobby')
     return render_template('login.html')
 
+def handling_request_lobby():
+    if request.method == 'POST':
+        room=request.form['new_room']
+        with open('rooms/' + room + ".txt", 'w') as f:
+            f.write(room)
+    rooms = os.listdir('rooms/') 
+    new_rooms = [x[:-4] for x in rooms]
+    return render_template('lobby.html',room_names=new_rooms)
+
+
+def handling_request_chat():
+    
+    return render_template('chat.html')
+
 
 @server.route('/', methods=['GET', 'POST'])
 def homePage():
@@ -45,6 +60,14 @@ def register():
 @server.route('/login', methods=['GET', 'POST'])
 def login():
     return handling_request_login()
+
+@server.route('/lobby', methods=['GET', 'POST'])
+def lobby():
+    return handling_request_lobby()
+
+@server.route('/chat/<room_name>', methods=['GET', 'POST'])
+def chat():
+    return handling_request_chat()
 
 
 
