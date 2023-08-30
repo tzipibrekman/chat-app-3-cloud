f8612e3387be2f57a601a2a743bc6a8efb43e612
rovrovayala <rinarov11@gmail.com>
2023-08-30 14:01:55 +0300

    deploy

--- del.sh
+++
@@ -1,8 +0,0 @@
-# Stop and remove the container
-read -p "enter container name: " cn
-read -p "enter img name: " img
-docker stop "$cn"
-docker rm "$cn"
-
-# Remove the Docker image
-docker rmi "$img"
