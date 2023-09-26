5f42777d9ab365d0d89ddd502f1790690da3be65
rovrovayala <rinarov11@gmail.com>
2023-09-26 14:23:30 +0300

    deploy-step2

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,10 +1,21 @@
-#!bin/bash
-if [ git rev-parse -q --verify "refs/tags/$1" ] && [ git cat-file -e "$1" ];then
-   git tag $1 $2
-   git push origin $1
+#!/bin/bash
 
-   docker build -t chat-app:$1 .
-   docker push chat-app:$1
-else
-   echo "wrong params"
+# Check if the image exists
+IMAGE_NAME="chat-app:$1"
+if docker image inspect $IMAGE_NAME > /dev/null 2>&1; then
+    # Prompt the user if they want to rebuild the image
+    echo "The image '$IMAGE_NAME' already exists. Do you want to rebuild it? (y/n)"
+    read REBUILD
+
+    # If the user chooses to rebuild the image, delete the existing one
+    if [[ "$REBUILD" == "y" ]]; then
+        echo "Deleting the existing image..."
+        docker image rm $IMAGE_NAME
+    fi
 fi
+
+# Build the image
+docker build -t $IMAGE_NAME .
+
+# Push the image to Docker Hub
+docker push $IMAGE_NAME
